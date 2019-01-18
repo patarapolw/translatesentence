5da8e3c4854dc6a02dfa64b2971cb8c168109eb0
patarapolw <patarapolw@gmail.com>
2019-01-18 17:09:13 +0700

    Add spacing between Pinyin

--- src/main/kotlin/translatesentence/CESentenceDictionary.kt
+++ src/main/kotlin/translatesentence/CESentenceDictionary.kt
@@ -1,15 +1,35 @@
 package translatesentence
 
+import com.huaban.analysis.jieba.JiebaSegmenter
 import net.duguying.pinyin.Pinyin
 
 class CESentenceDictionary (databaseUrl: String) {
     private val py = Pinyin()
     private val tatoeba = Tatoeba(databaseUrl)
+    private val jieba = JiebaSegmenter()
 
     data class CEResult (val chinese: String, val pinyin: String, val english: String)
 
     operator fun get(s: String): List<CEResult> {
         val result = tatoeba.search(s, from = "cmn", to = "eng")
-        return result.map { CEResult(chinese = it.from, pinyin = py.translate(it.from), english = it.to) }
+        return result.map { CEResult(chinese = it.from, pinyin = getPinyin(it.from), english = it.to) }
+    }
+
+    private fun getPinyin(s: String): String {
+        val output = mutableListOf<String>()
+
+        var previousIsHan = false
+        jieba.sentenceProcess(s).forEach { segment ->
+            if (Regex("\\p{IsHan}").containsMatchIn(segment)) {
+                if (previousIsHan) output.add(" ")
+                previousIsHan = true
+            } else {
+                previousIsHan = false
+            }
+
+            output.add(segment)
+        }
+
+        return py.translate(output.joinToString(""))
     }
 }
